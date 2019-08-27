c7b15455915274d3dc31c735ce04c9a5e3f7e052
William Clifford <williamdavidclifford@gmail.com>
2019-08-28 00:07:58 +1000

    Updates

---
+++ precache-manifest.c0117c4c49826b0388a153ded313defc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3b77b6bcaa8edaf2a3e31493b0815f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "22639749fb19909a92e0",
+    "url": "/Portfolio/static/css/2.9692cc1a.chunk.css"
+  },
+  {
+    "revision": "7a12331ef1c70beae86e",
+    "url": "/Portfolio/static/css/main.c038139a.chunk.css"
+  },
+  {
+    "revision": "22639749fb19909a92e0",
+    "url": "/Portfolio/static/js/2.66fe9d11.chunk.js"
+  },
+  {
+    "revision": "7a12331ef1c70beae86e",
+    "url": "/Portfolio/static/js/main.cb1a8c21.chunk.js"
+  },
+  {
+    "revision": "1174fe5c033da03a9932",
+    "url": "/Portfolio/static/js/runtime~main.70a681e8.js"
+  }
+]);
