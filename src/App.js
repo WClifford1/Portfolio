7754b4bca8068566970bd3e6689d5264920c22d1
William Clifford <williamdavidclifford@gmail.com>
2019-08-28 22:56:04 +1000

    centre heading on mobile

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Portfolio from './components/Portfolio';
 import TechStack from './components/TechStack';
 import Contact from './components/Contact';
 import 'antd/dist/antd.css';
-
+import './App.css'
 
 function App() {
     return (
         <React.Fragment>
-            <h1 style={{paddingLeft: "1%", paddingRight: "1%"}}>William Clifford | Junior Developer</h1>
+            <h1 className="heading" style={{paddingLeft: "1%", paddingRight: "1%"}}>William Clifford | Junior Developer</h1>
             <Aboutme />
             <Portfolio />
             <TechStack />
