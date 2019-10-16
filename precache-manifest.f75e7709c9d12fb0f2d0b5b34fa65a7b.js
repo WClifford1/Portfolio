0cf2696c3c17b503cb5a1682a8c2fa227d582154
William Clifford <williamdavidclifford@gmail.com>
2019-10-16 10:28:35 +1000

    Updates

--- precache-manifest.f75e7709c9d12fb0f2d0b5b34fa65a7b.js
+++ precache-manifest.f75e7709c9d12fb0f2d0b5b34fa65a7b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d2cd645cd740f2be35cebd6a108e8d6",
+    "revision": "8b26d03e6d11e872301fed273b4027c5",
     "url": "/Portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio/static/css/2.9692cc1a.chunk.css"
   },
   {
-    "revision": "cd83ff1902113d60941c",
+    "revision": "ceda7963dd93be278d86",
     "url": "/Portfolio/static/css/main.db76883b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio/static/js/2.610ca963.chunk.js"
   },
   {
-    "revision": "cd83ff1902113d60941c",
-    "url": "/Portfolio/static/js/main.7d8fc131.chunk.js"
+    "revision": "ceda7963dd93be278d86",
+    "url": "/Portfolio/static/js/main.0b8cb13e.chunk.js"
   },
   {
     "revision": "1174fe5c033da03a9932",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.7181ddbc1ee58bba8b9293de93625cf1.js"
+  "/Portfolio/precache-manifest.f75e7709c9d12fb0f2d0b5b34fa65a7b.js"
 );
 
 self.addEventListener('message', (event) => {
