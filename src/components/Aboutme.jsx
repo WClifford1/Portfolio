898dfd69c4f3c45ac44aa8c9df288875861a19df
William Clifford <williamdavidclifford@gmail.com>
2019-11-22 17:45:28 +1000

    update

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,30 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Aboutme extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dm: this.props.darkMode
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dm: this.props.darkMode
-        };
-    }
-
-    render() {
-        return (
-            <div style={{paddingLeft: "1%", paddingRight: "1%"}}>
-                <section className="hello">
-                    <h2 >About Me</h2>
-                    <br/>
-                    <p>
-                        I have recently completed a Diploma of Information Technology from the web-development bootcamp at Coderacademy in Brisbane. I have experience working with different languages, libraries and frameworks, building and working with RESTFul APIs and using the AGILE methodology throughout the entire software development life cycle. I have also built a full-stack web application for a real world client. 
-                        <br/><br/>
-                        In August 2019 I successfully completed an internship as a junior developer at Sortal in Brisbane, during which I worked on creating back-end micro-services and front-end UI components, predominantly using React, Typescript, Mongodb, Express, Node and Docker.
-                        <br/><br/>
-                        My goal is to work as a web developer at a company in which I am encouraged to perform well and is conducive to skills development and internal career progression. I am open to remote work and open to relocation.</p> 
-                </section>
-                <br/>
-                <br/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={{ paddingLeft: "1%", paddingRight: "1%" }}>
+        <section className="hello">
+          <h2>About Me</h2>
+          <br />
+          <p>
+            I have recently completed a Diploma of Information Technology from
+            the web-development bootcamp at Coderacademy in Brisbane, during which I gained
+            experience building RESTFul APIs using multiple languages, libraries
+            and frameworks and using the AGILE methodology throughout the
+            software development lifecycle.
+            <br /><br /> I have completed a Junior Developer
+            internship at Sortal in Brisbane, during which I worked on back-end
+            micro-services and front-end UI components, predominantly using
+            Typescript, React.js, Node.js, MongoDB and Docker.
+            <br /><br />I am currently seeking to work as a Junior Developer in a role
+            in which I am encouraged to perform well and which contributes to my
+            professional development and career progression. Please do not
+            hesitate to contact me if there is any further information I that
+            can provide or any coding challenges that I can complete to
+            demonstrate my suitability, thank you.
+          </p>
+        </section>
+        <br />
+        <br />
+      </div>
+    );
+  }
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -16,6 +16,7 @@ export default class Portfolio extends Component {
             mernApp: false,
             sortal: false,
             nutritionix: false,
+            nodeGoogle: false,
             index: 0,
             state: 0
         };
@@ -44,95 +45,85 @@ export default class Portfolio extends Component {
     openNutritionix = (e) => {
         this.setState({ nutritionix : !this.state.nutritionix})
     }
+
+    openNodeGoogle = (e) => {
+        this.setState({ nodeGoogle : !this.state.nodeGoogle})
+    }
     
     render() {
 
 
-        const {coderAc, rubyApp, railsApp, mernApp, sortal, nutritionix} = this.state;
+        const {coderAc, railsApp, mernApp, sortal, nutritionix, nodeGoogle} = this.state;
         return (
             <div style={{paddingLeft: "1%", paddingRight: "1%"}}>
-                <h2>Portfolio</h2>
+                <h2>Portfolio <span><h6>(Click to expand)</h6></span></h2>
                 <Timeline mode="alternate">
+
+                    <Timeline.Item onClick={() => this.openNodeGoogle()} color="green">
+                        <p className="timehead" style={{color:"black", cursor:"pointer"}}><Icon className="arrow" type="caret-right" style={sortal? {transform:"rotate(90deg)"} : undefined} /> Node.js Google Calendar API app - Oct 2019</p>
+                        <Collapse isOpened={nodeGoogle}>
+                                <p>Node.js booking system that uses the Google Calendar API</p>
+                                <p>Bookings can be made and timeslots viewed on Postman</p>
+                                <p>Created bookings will be added as an event in the users' Google Calendar</p>
+                                <p>Github: <a rel="noopener noreferrer" target="_blank" href="https://github.com/WClifford1/Nutrition-Calculator-App">https://github.com/WClifford1/Nutrition-Calculator-App</a></p>
+                        </Collapse>
+                    </Timeline.Item>
                     
 
                     <Timeline.Item onClick={() => this.openNutritionix()}>
-                        <p className="timehead" style={{color:"black", cursor:"pointer"}}><Icon className="arrow" type="caret-right" style={nutritionix? {transform:"rotate(90deg)"} : undefined} /> Nutritionix API web app Sep 2019</p>
-                        <Collapse isOpened={nutritionix} id="nut">
-                                <p>Single page app built entirely on React</p>
-                                <p>Uses Nutritionix API</p>
-                                <p>Fully responsive</p>
-                                <p>Deployment: Netlify</p>
-                                <a rel="noopener noreferrer" target="_blank" href="https://sad-golick-ae9bfd.netlify.com">https://sad-golick-ae9bfd.netlify.com</a>
-                                <br />
-                                <br />
-                        </Collapse>
-                        </Timeline.Item>
-
-
-                    <Timeline.Item onClick={() => this.opensortal()} color="green">
-                        <p className="timehead" style={{color:"black", cursor:"pointer"}}>Started internship with Sortal Aug 2019 <Icon className="arrow" type="caret-left" style={sortal? {transform:"rotate(-90deg)"} : undefined} /></p>
-                    <Collapse isOpened={sortal}>
-                                <p>Junior developer internship position</p>
-                                <p>Typescript, Docker, MERN stack</p>
-                                <p>Front-end and back-end projects</p>
-                                <p>Testing</p>
-                                <a rel="noopener noreferrer" target="_blank" href="https://sortal.io">https://sortal.io</a>
-                                <br />
-                                <br />
+                        <p className="timehead" style={{color:"black", cursor:"pointer"}}>Nutritionix API App - Sep 2019 <Icon className="arrow" type="caret-left" style={nutritionix? {transform:"rotate(-90deg)"} : undefined} /></p>
+                        <Collapse isOpened={nutritionix}>
+                                <p>Live Demo: <a rel="noopener noreferrer" target="_blank" href="https://sad-golick-ae9bfd.netlify.com">https://sad-golick-ae9bfd.netlify.com</a></p>
+                                <p>Github: <a rel="noopener noreferrer" target="_blank" href="https://github.com/WClifford1/Nutrition-Calculator-App">https://github.com/WClifford1/Nutrition-Calculator-App</a></p>
+                                <p>Responsive single page React.js app</p>
+                                <p id="hi">Uses the Nutritionix API to gather nutritional information on searched foods</p>
                         </Collapse>
                     </Timeline.Item>
 
 
-                    <Timeline.Item onClick={() => this.openmernApp()} dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>
-                        <p className="timehead" style={{color:"black", cursor:"pointer"}}><Icon className="arrow" type="caret-right" style={mernApp? {transform:"rotate(90deg)"} : undefined} /> Created Wotnot Media portfolio web app Jul 2019</p>
+                    <Timeline.Item onClick={() => this.opensortal()} dot={<Icon type="clock-circle-o" style={{ fontSize: '16px' }} />}>
+                        <p className="timehead" style={{color:"black", cursor:"pointer"}}><Icon className="arrow" type="caret-right" style={sortal? {transform:"rotate(90deg)"} : undefined} /> Junior Developer Internship at Sortal - Aug 2019</p>
+                        <Collapse isOpened={sortal}>
+                                <p><a rel="noopener noreferrer" target="_blank" href="https://sortal.io">https://sortal.io</a></p>
+                                <p>Worked on back-end microservices, front-end UI components and testing</p>
+                                <p>Typescript, Node.js, Express.js, React.js, MongoDB and Docker</p>
+                        </Collapse>
+                    </Timeline.Item>
+
+
+                    <Timeline.Item onClick={() => this.openmernApp()} color="purple">
+                        <p className="timehead" style={{color:"black", cursor:"pointer"}}>Wotnot Media Portfolio Web Page - Jul 2019 <Icon className="arrow" type="caret-left" style={mernApp? {transform:"rotate(-90deg)"} : undefined} /></p>
                         <Collapse isOpened={mernApp}>
-                                <p>Portfolio site for graphic design company</p>
-                                <p>Has working contact forms and admin dashboard</p>
-                                <p>Front-end: React</p>
-                                <p>Back-end: Node.js and Express</p>
-                                <p>Database: Mongodb</p>
+                                <p><a rel="noopener noreferrer" target="_blank" href="https://festive-cori-46ce58.netlify.com">https://festive-cori-46ce58.netlify.com</a></p>
+                                <p>Portfolio website for a graphic design company</p>
+                                <p>React.js, Node.js, Express.js and MongoDB</p>
                                 <p>Storage: AWS</p>
                                 <p>Deployment: Netlify and Heroku</p>
-                                <a rel="noopener noreferrer" target="_blank" href="https://festive-cori-46ce58.netlify.com">https://festive-cori-46ce58.netlify.com</a>
-                                <br />
-                                <br />
                         </Collapse>
                     </Timeline.Item>
 
 
                     <Timeline.Item onClick={() => this.openrailsApp()}>
-                        <p className="timehead" style={{color:"black", cursor:"pointer"}}>Created Ruby on Rails two-sided marketplace app May 2019 <Icon className="arrow" type="caret-left" style={railsApp? {transform:"rotate(-90deg)"} : undefined} /> </p>
+                        <p className="timehead" style={{color:"black", cursor:"pointer"}}><Icon className="arrow" type="caret-right" style={railsApp? {transform:"rotate(90deg)"} : undefined} /> Ruby on Rails Two-sided Marketplace App - May 2019</p>
                         <Collapse isOpened={railsApp}>
-                                <p>Ruby on Rails two-sided marketplace app</p>
+                                <p><a rel="noopener noreferrer" target="_blank" href="https://glacial-castle-71209.herokuapp.com">https://glacial-castle-71209.herokuapp.com</a></p>
+                                <p>Two-sided marketplace app - users can create an account, create listings, view listings, add items to shopping cart and purchase items.</p>
                                 <p>Database: PostgreSQL</p>
-                                <p>Deployment Platform: Heroku</p>
                                 <p>User Authentication: Devise</p>
+                                <p>Deployment: Heroku</p>
                                 <p>Payment: Stripe</p>
-                                <a rel="noopener noreferrer" target="_blank" href="https://glacial-castle-71209.herokuapp.com">https://glacial-castle-71209.herokuapp.com</a>
-                                <br />
-                                <br />
                         </Collapse>
                     </Timeline.Item>
                     
 
-                    <Timeline.Item onClick={() => this.openrubyApp()} color="red">
-                        <p className="timehead" style={{color:"black", cursor:"pointer"}}><Icon className="arrow" type="caret-right" style={rubyApp? {transform:"rotate(90deg)"} : undefined} /> Created first terminal app Mar 2019</p>
-                        <Collapse isOpened={rubyApp}>
-                                <p>Simple Ruby terminal app</p>
-                                <p>Demonstrate basic CRUD functionality</p>
-                                <a rel="noopener noreferrer" target="_blank" href="https://github.com/WClifford1/Ruby-Terminal-Assessment">https://github.com/WClifford1/Ruby-Terminal-Assessment</a>
-                                <br />
-                                <br />
-                        </Collapse>
-                    </Timeline.Item>
 
 
                     <Timeline.Item onClick={() => this.opencoderAc()} dot={<Icon type="clock-circle-o" style={{ color:"#1890ff",fontSize: '16px' }} />}>
                         <p className="timehead" style={{color:"black", cursor:"pointer"}}>Started Coderacademy Mar 2019 <Icon className="arrow" type="caret-left" style={coderAc? {transform:"rotate(-90deg)"} : undefined} /> </p>
                             <Collapse isOpened={coderAc}>
-                                <p>Web development bootcamp</p>
+                                <p><a rel="noopener noreferrer" target="_blank" href="https://www.coderacademy.edu.au">https://www.coderacademy.edu.au</a></p>
+                                <p>Full-time Web development bootcamp</p>
                                 <p>Diploma of Information Technology</p>
-                                <a rel="noopener noreferrer" target="_blank" href="https://www.coderacademy.edu.au">https://www.coderacademy.edu.au</a>
                             </Collapse>
                     </Timeline.Item>
                 </Timeline>
